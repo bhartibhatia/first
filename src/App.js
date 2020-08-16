28ae2523875bf34687773daa2cc5336a3383c92c
bhartibhatia <bhartibhatia1997@gmail.com>
2020-08-16 15:29:35 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ var photo = require('./assets/img.JPG')
 function App() {
   return (
      <div className = "container">
-        <h1>Hello World</h1>
+        {/* <h1>Hello World</h1>
         <h1>Hello Screen</h1>
         <h1>Hello Bharti</h1>
         <div>
-          {/* <Starting/> */}
+          <Starting/>
         <Starting  MyPhoto={photo}/>
         
-        </div>
+        </div> */}
+        {/* <img src={require('./assets/code.jpg')}/> */}
+        <h1>Hello Screen</h1>
         </div>
          );
   

75da68c81624f470adb79ae89d4fb08bad09f4b3
bhartibhatia <bhartibhatia1997@gmail.com>
2020-08-16 16:58:59 +0530

    For Each Loop Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import './styles.css'
-import {Starting} from './starting'
-var photo = require('./assets/img.JPG')
+var myimg = require('./assets/img.JPG')
+var mydata = [
+  { name: "emp1", desg: "emp1desg", imageurl: myimg },
+  { name: "emp2", desg: "emp2desg", imageurl: myimg },
+  { name: "emp3", desg: "emp3desg", imageurl: myimg },
+  { name: "emp4", desg: "emp4desg", imageurl: myimg },
+  { name: "emp5", desg: "emp5desg", imageurl: myimg }
+];
+
+function Bha(str1, str2, imageurl) {
+  return (
+    <div className="Bha">
+      <img className="Photo" src={imageurl} alt={"Hello"} />
+      <h3 className="str1">{str1}</h3>
+      <h4 className="str1">{str2}</h4>
+    </div>
+  );
+} 
 
 function App() {
+  
   return (
      <div className = "container">
-        {/* <h1>Hello World</h1>
-        <h1>Hello Screen</h1>
-        <h1>Hello Bharti</h1>
-        <div>
-          <Starting/>
-        <Starting  MyPhoto={photo}/>
-        
-        </div> */}
-        {/* <img src={require('./assets/code.jpg')}/> */}
-        <h1>Hello Screen</h1>
-        </div>
+       {
+         mydata.forEach(element => {
+           console.log(element.name);
+           console.log(element.imageurl);
+         })
+       }
+       {/* {Bha('bharti','bhatia',myimg)}
+       {Bha('chinki','bhatia',myimg)}
+       <Bha str1={'bharti'} str2={'bhatia'} imageurl={myimg}/> */}
+       </div>
          );
+         
   
 }
 
